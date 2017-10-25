c92f4448ad468e8169ab50d233c2e1670d607a8c
Ted S <claimdotred@gmail.com>
2017-10-25 23:54:42 +0300

    frog jmp task

---
+++ cpp/Frog-Jmp.cpp
@@ -0,0 +1,37 @@
+﻿
+/* codility - FrogJmp */
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <cassert>
+
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+
+
+int solution(int X, int Y, int D) 
+{
+   int rem = (Y - X) % D, res = (Y - X) / D;
+
+   if (rem != 0)
+      return res + 1;
+   else
+      return res;
+}
+
+int main(int argc, char *argv[])
+{
+   static const int arr[] = {3, 1, 2, 4, 3};
+   std::vector<int> a(arr, arr + sizeof(arr) / sizeof(arr[0]));
+
+   int res = solution(a);
+
+   printf("%i\n", res);
+
+
+
+   return EXIT_SUCCESS;
+}
