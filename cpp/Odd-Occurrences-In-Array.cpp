de5114ea54bc91bd2776dddf8552591bfc7f6bfb
Ted S <claimdotred@gmail.com>
2017-10-25 20:22:19 +0300

    neu

---
+++ cpp/Odd-Occurrences-In-Array.cpp
@@ -0,0 +1,78 @@
+/*A non-empty zero-indexed array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+For example, in array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the elements at indexes 0 and 2 have value 9,
+the elements at indexes 1 and 3 have value 3,
+the elements at indexes 4 and 6 have value 9,
+the element at index 5 has value 7 and is unpaired.
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the function should return 7, as explained in the example above.
+
+Assume that:
+
+N is an odd integer within the range [1..1,000,000];
+each element of array A is an integer within the range [1..1,000,000,000];
+all but one of the values in A occur an even number of times.
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).*/
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <cassert>
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> &A) {
+   
+   // write your code in C++14 (g++ 6.2.0)
+
+   std::sort(A.begin(), A.end());
+
+   int i = 0, k = 0, n = A.size();
+   int val = A[i];
+
+   while (true)
+   {
+      val = A[i], k = 0;
+
+      while (A[i] == val && i < n)
+         i++, k++;
+
+      if (k % 2 != 0)
+         return val;
+   }
+ 
+   return -1;
+}
+
+int main(int argc, char *argv[])
+{
+   static const int arr[] = {9, 3, 9, 3, 9, 7, 9};
+   std::vector<int> a(arr, arr + sizeof(arr) / sizeof(arr[0]));
+
+   printf("%i", solution(a));
+
+
+
+   return EXIT_SUCCESS;
+}
--- cpp/binary-gap.cpp
+++ cpp/binary-gap.cpp
@@ -3,66 +3,19 @@
 
 #include <cassert>
 
-/* A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+#include <algorithm>
 
-For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+int solution(vector<int> &A) {
+   // write your code in C++14 (g++ 6.2.0)
 
-Write a function:
+   A.sort();
 
-int solution(int N);
-that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap. */
 
-int solve( const int c ) 
-{
-   int n = c, res = 0, t = 0;
-   bool trail = false;
-
-   while (n)
-   {
-      if (n & 1) 
-      {
-         if (!trail)
-            trail = true;
-         else if (trail) 
-         {
-            if (t > res)
-               res = t;
-            t = 0;
-         }
-      }
-      else
-      {
-         if (trail)
-            t++;
-      }
-
-      n >>= 1;
-   }
-
-   return res;
 }
-
 
 int main( int argc, char *argv[] ) 
 {
-   int a, res, ans;
-
-   solve(74901729);
-
-   a = 20, res = solve(a), ans = 1;
-   assert(res == ans);
-
-   a = 529, res = solve(a), ans = 4;
-   assert(res == ans);
-
-   a = 1041, res = solve(a), ans = 5;
-   assert(res == ans);
-
-   a = 15, res = solve(a), ans = 0;
-   assert(res == ans);
-
-   printf("%i", res);
-
+   sort([9, 3, 9, 3, 9, 7, 9]);
 
    return EXIT_SUCCESS;
 }
